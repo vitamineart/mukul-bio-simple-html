7f0c1bc54d35120354d1eb7856a16bc1646d168c
Art <vitaminearte@gmail.com>
2021-03-03 14:03:32 +0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+const faders = document.querySelectorAll('.text-content .fade-content *');
+faders.forEach( el => el.classList.toggle('fade-in'));
+
+const appearOptions = {
+  threshold: .8
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
